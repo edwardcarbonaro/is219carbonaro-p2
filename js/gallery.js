887e1f3ae3f1651bfb198f1026cc6bde908597c6
Edward Carbonaro <edward.carbonaro12@gmail.com>
2017-04-07 20:00:07 -0400

    Add getQueryParams function

--- js/gallery.js
+++ js/gallery.js
@@ -112,6 +112,22 @@ window.addEventListener('load', function() {
 
 }, false);
 
+
+
+function getQueryParams(qs) {
+ qs = qs.split("+").join(" ");
+ var params = {},
+ tokens,
+ re = /[?&]?([^=]+)=([^&]*)/g;
+ while (tokens = re.exec(qs)) {
+ params[decodeURIComponent(tokens[1])]
+ = decodeURIComponent(tokens[2]);
+ }
+ return params;
+}
+var $_GET = getQueryParams(document.location.search);
+console.log($_GET["fname"]); // would output "John"
+
 function GalleryImage(location,description,date,img) {
 	this.location = location;
 	this.description = description;
